8fe03309aba50036fc04d48d821e8d42a66e26db
Lingming Xia <xialingming@gmail.com>
2021-06-04 14:47:10 +0800

    add audio feature speak

--- main.py
+++ main.py
@@ -5,6 +5,8 @@
 from snowboy import snowboydecoder
 from robot import utils, Player, constants, logging
 
+interrupted = False
+
 logger = logging.getLogger(__name__)
 
 
@@ -12,18 +14,30 @@ def detected_callback():
     def start_record():
         utils.setRecordable(True)
         logger.info('开始录音')
-
+    # first disable audio record.
+    utils.setRecordable(False)
+    # no after player played the startup beep.
     Player.play(constants.getData('beep_hi.wav'), onCompleted=start_record, wait=True)
 
 
 def audioRecorderCallback(fname):
     print(fname)
+    utils.clean()
+    global interrupted
+    interrupted = True
+
+
+def _interrupt_callback(self):
+    global interrupted
+    return interrupted
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     detector = snowboydecoder.HotwordDetector("snowboy/resources/zijinshan.pmdl", sensitivity=0.5, audio_gain=1)
     detector.start(detected_callback=detected_callback,
-                   audio_recorder_callback=audioRecorderCallback)
+                   audio_recorder_callback=audioRecorderCallback,
+                   interrupt_check=_interrupt_callback,
+                   )
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
